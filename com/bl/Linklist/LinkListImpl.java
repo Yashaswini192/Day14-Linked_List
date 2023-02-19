395168342c1fdffe606da6bd26e174ae0239928c
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-19 16:16:34 +0530

    creating a method to add values at the beginning of the list

--- com/bl/Linklist/LinkListImpl.java
+++ com/bl/Linklist/LinkListImpl.java
@@ -5,7 +5,21 @@ public class LinkListImpl<T> {
 	Node temp;
 	Node tail;
 	
+
 	public void add(T key) {
+		Node<T>node = new Node<>(key);
+		if(head == null) {
+			head = node;
+			return;
+		}
+		temp = head;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=node;
+	}
+	
+	public void insertAtStart(T key) {
 		Node<T>node = new Node<>(key);
 		if(head == null) {
 			head = node;
@@ -14,20 +28,12 @@ public class LinkListImpl<T> {
 		}
 		temp = head;
 		head = node;
-		//while(temp.next!=null) {
-		//	temp=temp.next;
-		//}
 		head.next = temp;
-		//temp.next = tail;
-		
 	}
 
 	@Override
 	public String toString() {
 		return "LinkListImpl [head=" + head + " , temp=" + temp + ", tail=" + tail + "]";
 	}
-
-
-
 	
 }
--- com/bl/Linklist/LinkListMain.java
+++ com/bl/Linklist/LinkListMain.java
@@ -4,11 +4,14 @@ public class LinkListMain {
 
 	public static void main(String[] args) {
 		LinkListImpl  L1 = new LinkListImpl();
-		L1.add(70);
-		//System.out.println(L1);
-		L1.add(30);
-		//System.out.println(L1);
-		L1.add(56);
+		/*
+		 * L1.add(70); L1.add(30); L1.add(56);
+		 */
+		
+		L1.insertAtStart(70);
+		L1.insertAtStart(30);
+		L1.insertAtStart(56);
+		
 		System.out.println(L1);
 		
 	}
